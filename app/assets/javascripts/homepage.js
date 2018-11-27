5d686b7ad60cd4d04288731823ffd0e60bc24c60
Felipe Santos <santos.felipe13@gmail.com>
2018-11-27 16:52:10 -0200

    homepage done

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -8,6 +8,16 @@ bannerFirstLine = document.getElementsByClassName("main-title-text-line")[0]
 bannerSecondLine = document.getElementsByClassName("main-title-text-line")[1]
 bannerThirdLine = document.getElementsByClassName("main-title-text-line")[2]
 
+solutionCards = document.getElementsByClassName("solutions-card")
+
+teamMembersAll = document.getElementsByClassName("team-description-flex")
+teamMembersFirstRow = []
+teamMembersFirstRow.push(teamMembersAll[0])
+teamMembersFirstRow.push(teamMembersAll[1])
+teamMembersSecondRow = []
+teamMembersSecondRow.push(teamMembersAll[2])
+teamMembersSecondRow.push(teamMembersAll[3])
+
 navbarComponent = document.getElementsByClassName("navbar-wagon")[0]
 
 body = document.getElementsByTagName("body")[0]
@@ -32,7 +42,32 @@ setTimeout(function(){
   }, 300);
 }, 300);
 
+//Scroll solutions scroll-in
+window.addEventListener('scroll', function() {
+  if (window.pageYOffset > 320) {
+    Array.prototype.forEach.call(solutionCards, card => {
+      card.classList.add("come-in");
+    });
+  }
+});
 
+//Scroll team scroll-in first row
+window.addEventListener('scroll', function() {
+  if (window.pageYOffset > 850) {
+    Array.prototype.forEach.call(teamMembersFirstRow, card => {
+      card.classList.add("come-in");
+    });
+  }
+});
+
+//Scroll team scroll-in second row
+window.addEventListener('scroll', function() {
+  if (window.pageYOffset > 1150) {
+    Array.prototype.forEach.call(teamMembersSecondRow, card => {
+      card.classList.add("come-in");
+    });
+  }
+});
 
 
 });
